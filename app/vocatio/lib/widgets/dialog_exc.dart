88abde4ff4834cd282e4bfb9ceffdd6d9c948fd0
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-10-17 16:28:48 -0300

    fix: correcoes de cor

--- app/vocatio/lib/widgets/dialog_exc.dart
+++ app/vocatio/lib/widgets/dialog_exc.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Future<bool?> showDeleteDialog(BuildContext context, VoidCallback apagar) async {
  
-  final theme = Theme.of(context);
+  final ThemeData theme = Theme.of(context);
 
   return showDialog<bool>(
     context: context,
@@ -13,14 +13,14 @@ Future<bool?> showDeleteDialog(BuildContext context, VoidCallback apagar) async 
           borderRadius: BorderRadius.circular(20),
         ),
         child: Container(
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             borderRadius: BorderRadius.all(Radius.circular(20)),
             gradient: LinearGradient(
               begin: Alignment.centerLeft,
               end: Alignment.centerRight,
               colors: [
-                Color(0xff1f182a),
-                Color.fromARGB(255, 70, 36, 121),
+                theme.colorScheme.onPrimaryFixed,
+                theme.colorScheme.onPrimaryFixedVariant
               ],
             ),
           ),
@@ -28,31 +28,30 @@ Future<bool?> showDeleteDialog(BuildContext context, VoidCallback apagar) async 
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const Icon(
+              Icon(
                 Icons.delete_forever,
-                color: Colors.white,
+                color: theme.colorScheme.primaryFixed,
                 size: 48,
               ),
-              const SizedBox(height: 16),
+              SizedBox(height: 16),
               Text(
                 'Deseja apagar essa turma?',
                 textAlign: TextAlign.center,
                 style: theme.textTheme.titleLarge?.copyWith(
-                  color: Colors.white,
+                  color: theme.colorScheme.primaryFixedDim,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(height: 12),
+              SizedBox(height: 12),
               Text(
                 'Não será possível restaurar essa turma, '
                 'deve ter certeza que deseja excluir permanentemente.',
                 textAlign: TextAlign.center,
                 style: theme.textTheme.bodyMedium?.copyWith(
-                  color: Colors.white.withOpacity(0.9),
-                  height: 1.4,
+                  color: theme.colorScheme.primaryFixed,
                 ),
               ),
-              const SizedBox(height: 24),
+              SizedBox(height: 24),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -61,18 +60,18 @@ Future<bool?> showDeleteDialog(BuildContext context, VoidCallback apagar) async 
                     child: Text(
                       'Cancelar',
                       style: theme.textTheme.bodyLarge?.copyWith(
-                        color: Colors.white.withOpacity(0.9),
+                        color: theme.colorScheme.primaryFixedDim
                       ),
                     ),
                   ),
-                  const SizedBox(width: 24),
+                  SizedBox(width: 24),
                   TextButton(
                     onPressed: () => {Navigator.pop(context, true),apagar},
                 
                     child: Text(
                       'Apagar',
                       style: theme.textTheme.bodyLarge?.copyWith(
-                        color: Colors.redAccent,
+                        color: theme.colorScheme.error,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
