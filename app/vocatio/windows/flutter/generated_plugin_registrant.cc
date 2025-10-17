e8c0335088a6ec87986e222bf84305b91d06c2cb
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-10-17 16:51:22 -0300

    fix: correcoes pos merge

--- app/vocatio/windows/flutter/generated_plugin_registrant.cc
+++ app/vocatio/windows/flutter/generated_plugin_registrant.cc
@@ -6,15 +6,15 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <geolocator_windows/geolocator_windows.h>
 #include <firebase_auth/firebase_auth_plugin_c_api.h>
 #include <firebase_core/firebase_core_plugin_c_api.h>
+#include <geolocator_windows/geolocator_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  GeolocatorWindowsRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   FirebaseAuthPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseAuthPluginCApi"));
   FirebaseCorePluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FirebaseCorePluginCApi"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
 }
