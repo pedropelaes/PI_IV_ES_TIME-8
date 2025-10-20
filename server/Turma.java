a6e4f4e7df419ce0bc47dcb2ed47fee44bcb8590
Pedro Facine Nery <penery06@gmail.com>
2025-10-20 17:54:23 -0300

    Feat: OperaçãoCriarTurma

--- server/Turma.java
+++ server/Turma.java
@@ -1,4 +1,29 @@
 public class Turma {
     private String nome;
     private String codigo;
+    private String professorId;
+    private String[] alunos;
+    private String criadoEm;
+    private String atualizadoEm;
+
+    public Turma(String nome, String codigo, String professorId, String[] alunos, String criadoEm, String atualizadoEm) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.professorId = professorId;
+        this.alunos = alunos;
+        this.criadoEm = criadoEm;
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    @Override
+    public String toString() {
+        return "Turma{" +
+                "nome='" + nome + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", professorId='" + professorId + '\'' +
+                ", alunos=" + java.util.Arrays.toString(alunos) +
+                ", criadoEm='" + criadoEm + '\'' +
+                ", atualizadoEm='" + atualizadoEm + '\'' +
+                '}';
+    }
 }
