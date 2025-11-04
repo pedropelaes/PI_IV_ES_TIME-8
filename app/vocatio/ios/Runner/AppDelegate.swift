59fe50d2815eb6a7c0ac1426134e3d1db84c0952
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-11-04 10:24:43 -0300

    fix: chave limitada

--- app/vocatio/ios/Runner/AppDelegate.swift
+++ app/vocatio/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY") as? String else {
+    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "AIzaSyDDu4JrvVqczSIIRILMmV4j1HWJsKXERKc") as? String else {
       fatalError("API Key 'GOOGLE_MAPS_API_KEY' não encontrada no Info.plist")
     }
     
