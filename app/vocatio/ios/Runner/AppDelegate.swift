8e11dcc44300829c569733df1528e76f8b481d56
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-11-04 18:52:59 -0300

    fix: ios build

--- app/vocatio/ios/Runner/AppDelegate.swift
+++ app/vocatio/ios/Runner/AppDelegate.swift
@@ -8,11 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "AIzaSyAyR-xVoIbkX3mQH9577iI1My28xM9a8HE") as? String else {
-      fatalError("API Key 'GOOGLE_MAPS_API_KEY' não encontrada no Info.plist")
-    }
+      GMSServices.provideAPIKey("AIzaSyAyR-xVoIbkX3mQH9577iI1My28xM9a8HE")
     
-    GMSServices.provideAPIKey(apiKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
